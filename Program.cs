ba9b52b4f0c585040dca068f1dd25f2f66b31b0b
romus <romus@DESKTOP-594KMDG>
2022-09-25 18:39:19 +0500

    Фикс сохранения файла

--- Program.cs
+++ Program.cs
@@ -7,96 +7,156 @@ namespace LINQ2
     {
 
         public static void createXML (string path) {
-            XDocument xmlDoc =
-            new XDocument(
-            new XDeclaration("1.0", "utf-8", "yes"),
-            new XComment("Кино"),
-            new XElement("Кинотеатры",
+            XDocument xmlDoc = new XDocument(
+                new XDeclaration("1.0", "utf-8", "yes"),
+                new XComment("Кино"),
+                new XElement("Кинотеатр_Мелодия",
 
-                new XElement("Кинотеатр", new XAttribute("Название", "Мелодия"),
+                        new XElement("Сеансы",
 
-                    new XElement("Сеансы",
+                            new XElement("Сеанс",
+                                new XAttribute("Дата", "19.09.2022"),
+                                new XAttribute("Время", "08:00"),
+                                new XAttribute("Цена_билета", "250"),
+                                new XAttribute("ID_Фильма", "1"),
+                                new XElement("Места",
+                                    new XElement("Место", new XAttribute("Номер", "1"), new XAttribute("Занято", "0")),
+                                    new XElement("Место", new XAttribute("Номер", "2"), new XAttribute("Занято", "1")),
+                                    new XElement("Место", new XAttribute("Номер", "3"), new XAttribute("Занято", "1")),
+                                    new XElement("Место", new XAttribute("Номер", "4"), new XAttribute("Занято", "1")),
+                                    new XElement("Место", new XAttribute("Номер", "5"), new XAttribute("Занято", "0")),
+                                    new XElement("Место", new XAttribute("Номер", "6"), new XAttribute("Занято", "1")),
+                                    new XElement("Место", new XAttribute("Номер", "7"), new XAttribute("Занято", "1")),
+                                    new XElement("Место", new XAttribute("Номер", "8"), new XAttribute("Занято", "0")),
+                                    new XElement("Место", new XAttribute("Номер", "9"), new XAttribute("Занято", "1")),
+                                    new XElement("Место", new XAttribute("Номер", "10"), new XAttribute("Занято", "1"))
+                                )
+                            ),
+                            new XElement("Сеанс",
+                                new XAttribute("Дата", "19.09.2022"),
+                                new XAttribute("Время", "10:00"),
+                                new XAttribute("Цена_билета", "270"),
+                                new XAttribute("ID_Фильма", "2"),
+                                new XElement("Места",
+                                    new XElement("Место", new XAttribute("Номер", "1"), new XAttribute("Занято", "1")),
+                                    new XElement("Место", new XAttribute("Номер", "2"), new XAttribute("Занято", "1")),
+                                    new XElement("Место", new XAttribute("Номер", "3"), new XAttribute("Занято", "1")),
+                                    new XElement("Место", new XAttribute("Номер", "4"), new XAttribute("Занято", "1")),
+                                    new XElement("Место", new XAttribute("Номер", "5"), new XAttribute("Занято", "1")),
+                                    new XElement("Место", new XAttribute("Номер", "6"), new XAttribute("Занято", "1")),
+                                    new XElement("Место", new XAttribute("Номер", "7"), new XAttribute("Занято", "1")),
+                                    new XElement("Место", new XAttribute("Номер", "8"), new XAttribute("Занято", "0")),
+                                    new XElement("Место", new XAttribute("Номер", "9"), new XAttribute("Занято", "1")),
+                                    new XElement("Место", new XAttribute("Номер", "10"), new XAttribute("Занято", "1"))
+                                )
+                            ),
+                            new XElement("Сеанс",
+                                new XAttribute("Дата", "19.09.2022"),
+                                new XAttribute("Время", "13:00"),
+                                new XAttribute("Цена_билета", "300"),
+                                new XAttribute("ID_Фильма", "3"),
+                                new XElement("Места",
+                                    new XElement("Место", new XAttribute("Номер", "1"), new XAttribute("Занято", "0")),
+                                    new XElement("Место", new XAttribute("Номер", "2"), new XAttribute("Занято", "0")),
+                                    new XElement("Место", new XAttribute("Номер", "3"), new XAttribute("Занято", "1")),
+                                    new XElement("Место", new XAttribute("Номер", "4"), new XAttribute("Занято", "1")),
+                                    new XElement("Место", new XAttribute("Номер", "5"), new XAttribute("Занято", "0")),
+                                    new XElement("Место", new XAttribute("Номер", "6"), new XAttribute("Занято", "0")),
+                                    new XElement("Место", new XAttribute("Номер", "7"), new XAttribute("Занято", "1")),
+                                    new XElement("Место", new XAttribute("Номер", "8"), new XAttribute("Занято", "0")),
+                                    new XElement("Место", new XAttribute("Номер", "9"), new XAttribute("Занято", "1")),
+                                    new XElement("Место", new XAttribute("Номер", "10"), new XAttribute("Занято", "1")),
+                                    new XElement("Место", new XAttribute("Номер", "11"), new XAttribute("Занято", "1")),
+                                    new XElement("Место", new XAttribute("Номер", "12"), new XAttribute("Занято", "0"))
+                                )
+                            ),
+                            new XElement("Сеанс",
+                                new XAttribute("Дата", "19.09.2022"),
+                                new XAttribute("Время", "15:00"),
+                                new XAttribute("Цена_билета", "270"),
+                                new XAttribute("ID_Фильма", "1"),
+                                new XElement("Места",
+                                    new XElement("Место", new XAttribute("Номер", "1"), new XAttribute("Занято", "1")),
+                                    new XElement("Место", new XAttribute("Номер", "2"), new XAttribute("Занято", "1")),
+                                    new XElement("Место", new XAttribute("Номер", "3"), new XAttribute("Занято", "1")),
+                                    new XElement("Место", new XAttribute("Номер", "4"), new XAttribute("Занято", "1")),
+                                    new XElement("Место", new XAttribute("Номер", "5"), new XAttribute("Занято", "0")),
+                                    new XElement("Место", new XAttribute("Номер", "6"), new XAttribute("Занято", "1")),
+                                    new XElement("Место", new XAttribute("Номер", "7"), new XAttribute("Занято", "0")),
+                                    new XElement("Место", new XAttribute("Номер", "8"), new XAttribute("Занято", "0")),
+                                    new XElement("Место", new XAttribute("Номер", "9"), new XAttribute("Занято", "1")),
+                                    new XElement("Место", new XAttribute("Номер", "10"), new XAttribute("Занято", "1")),
+                                    new XElement("Место", new XAttribute("Номер", "11"), new XAttribute("Занято", "1")),
+                                    new XElement("Место", new XAttribute("Номер", "12"), new XAttribute("Занято", "0")),
+                                    new XElement("Место", new XAttribute("Номер", "13"), new XAttribute("Занято", "1")),
+                                    new XElement("Место", new XAttribute("Номер", "14"), new XAttribute("Занято", "1")),
+                                    new XElement("Место", new XAttribute("Номер", "15"), new XAttribute("Занято", "1"))
+                                )
+                            ),
+                            new XElement("Сеанс",
+                                new XAttribute("Дата", "19.09.2022"),
+                                new XAttribute("Время", "18:00"),
+                                new XAttribute("Цена_билета", "270"),
+                                new XAttribute("ID_Фильма", "2"),
+                                new XElement("Места",
+                                    new XElement("Место", new XAttribute("Номер", "1"), new XAttribute("Занято", "1")),
+                                    new XElement("Место", new XAttribute("Номер", "2"), new XAttribute("Занято", "0")),
+                                    new XElement("Место", new XAttribute("Номер", "3"), new XAttribute("Занято", "1")),
+                                    new XElement("Место", new XAttribute("Номер", "4"), new XAttribute("Занято", "1")),
+                                    new XElement("Место", new XAttribute("Номер", "5"), new XAttribute("Занято", "0")),
+                                    new XElement("Место", new XAttribute("Номер", "6"), new XAttribute("Занято", "1")),
+                                    new XElement("Место", new XAttribute("Номер", "7"), new XAttribute("Занято", "1")),
+                                    new XElement("Место", new XAttribute("Номер", "8"), new XAttribute("Занято", "1")),
+                                    new XElement("Место", new XAttribute("Номер", "9"), new XAttribute("Занято", "1")),
+                                    new XElement("Место", new XAttribute("Номер", "10"), new XAttribute("Занято", "1")),
+                                    new XElement("Место", new XAttribute("Номер", "11"), new XAttribute("Занято", "1")),
+                                    new XElement("Место", new XAttribute("Номер", "12"), new XAttribute("Занято", "1")),
+                                    new XElement("Место", new XAttribute("Номер", "13"), new XAttribute("Занято", "1")),
+                                    new XElement("Место", new XAttribute("Номер", "14"), new XAttribute("Занято", "1")),
+                                    new XElement("Место", new XAttribute("Номер", "15"), new XAttribute("Занято", "0")),
+                                    new XElement("Место", new XAttribute("Номер", "16"), new XAttribute("Занято", "1")),
+                                    new XElement("Место", new XAttribute("Номер", "17"), new XAttribute("Занято", "0")),
+                                    new XElement("Место", new XAttribute("Номер", "18"), new XAttribute("Занято", "0")),
+                                    new XElement("Место", new XAttribute("Номер", "19"), new XAttribute("Занято", "1")),
+                                    new XElement("Место", new XAttribute("Номер", "20"), new XAttribute("Занято", "1"))
+                                )
+                            )
 
-                        new XElement("Сеанс",
-                            new XAttribute("Дата", "19.09.2022"),
-                            new XAttribute("Время", "08:00"),
-                            new XAttribute("Цена_билета", "250"),
-                            new XAttribute("ID_Фильма", "1"),
-                            new XAttribute("Мест", "30"),
-                            new XAttribute("Занято_мест", "22")
                         ),
-                        new XElement("Сеанс",
-                            new XAttribute("Дата", "19.09.2022"),
-                            new XAttribute("Время", "10:00"),
-                            new XAttribute("Цена_билета", "270"),
-                            new XAttribute("ID_Фильма", "2"),
-                            new XAttribute("Мест", "30"),
-                            new XAttribute("Занято_мест", "12")
-                        ),
-                        new XElement("Сеанс",
-                            new XAttribute("Дата", "19.09.2022"),
-                            new XAttribute("Время", "13:00"),
-                            new XAttribute("Цена_билета", "300"),
-                            new XAttribute("ID_Фильма", "3"),
-                            new XAttribute("Мест", "30"),
-                            new XAttribute("Занято_мест", "29")
-                        ),
-                        new XElement("Сеанс",
-                            new XAttribute("Дата", "19.09.2022"),
-                            new XAttribute("Время", "15:00"),
-                            new XAttribute("Цена_билета", "270"),
-                            new XAttribute("ID_Фильма", "1"),
-                            new XAttribute("Мест", "30"),
-                            new XAttribute("Занято_мест", "27")
-                        ),
-                        new XElement("Сеанс",
-                            new XAttribute("Дата", "19.09.2022"),
-                            new XAttribute("Время", "18:00"),
-                            new XAttribute("Цена_билета", "270"),
-                            new XAttribute("ID_Фильма", "2"),
-                            new XAttribute("Мест", "30"),
-                            new XAttribute("Занято_мест", "25")
+
+                        new XElement("Фильмы",
+                            new XElement("Фильм", new XAttribute("Название", "Чужой"), new XAttribute("id", "1"),
+                                new XElement("Режиссер",
+                                    new XAttribute("Фамилия", "Скотт"),
+                                    new XAttribute("Имя", "Ридли")
+                                ),
+                                new XElement("Издание",
+                                    new XAttribute("Год", "1979"),
+                                    new XAttribute("Длительность", "116")
+                                )
+                            ),
+                            new XElement("Фильм", new XAttribute("Название", "Аватар"), new XAttribute("id", "2"),
+                                new XElement("Режиссер",
+                                    new XAttribute("Фамилия", "Кэмерон"),
+                                    new XAttribute("Имя", "Джеймс")
+                                ),
+                                new XElement("Издание",
+                                    new XAttribute("Год", "2009"),
+                                    new XAttribute("Длительность", "162")
+                                )
+                            ),
+                            new XElement("Фильм", new XAttribute("Название", "Назад в будущее"), new XAttribute("id", "3"),
+                                new XElement("Режиссер",
+                                    new XAttribute("Фамилия", "Земекис"),
+                                    new XAttribute("Имя", "Роберт")
+                                ),
+                                new XElement("Издание",
+                                    new XAttribute("Год", "1985"),
+                                    new XAttribute("Длительность", "116")
+                                )
+                            )
                         )
-
                     )
-
-                 )
-
-            ),
-
-            new XElement("Фильмы",
-                new XElement("Фильм", new XAttribute("Название", "Чужой"), new XAttribute("id", "1"),
-                    new XElement("Режиссер",
-                        new XAttribute("Фамилия", "Скотт"),
-                        new XAttribute("Имя", "Ридли")
-                    ),
-                    new XElement("Издание",
-                        new XAttribute("Год", "1979"),
-                        new XAttribute("Длительность", "116")
-                    )
-                ),
-                new XElement("Фильм", new XAttribute("Название", "Аватар"), new XAttribute("id", "2"),
-                    new XElement("Режиссер",
-                        new XAttribute("Фамилия", "Кэмерон"),
-                        new XAttribute("Имя", "Джеймс")
-                    ),
-                    new XElement("Издание",
-                        new XAttribute("Год", "2009"),
-                        new XAttribute("Длительность", "162")
-                    )
-                ),
-                new XElement("Фильм", new XAttribute("Название", "Назад в будущее"), new XAttribute("id", "3"),
-                    new XElement("Режиссер",
-                        new XAttribute("Фамилия", "Земекис"),
-                        new XAttribute("Имя", "Роберт")
-                    ),
-                    new XElement("Издание",
-                        new XAttribute("Год", "1985"),
-                        new XAttribute("Длительность", "116")
-                    )
-                )
-            )
-
             );
             xmlDoc.Save(path);
         }
